4abc2622999a0a56bdcbf4035b15b832edb53a19
Terry Harmon <tharmon0717@gmail.com>
2017-02-23 11:35:17 -0600

    Start to front end test

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+var gulp            = require('gulp');
+var notify          = require('gulp-notify');
+var sass            = require('gulp-sass');
+var concat          = require('gulp-concat');
+var uglify          = require('gulp-uglify');
+var rev             = require('gulp-rev');
+var gutil           = require('gulp-util');
+var rimraf          = require('rimraf');
+var revOutdated     = require('gulp-rev-outdated');
+var path            = require('path');
+var through         = require('through2');
+var runSequence     = require('run-sequence');
+
+var assetsDir = 'resources/assets';
+var publicAssetsDir = 'public';
+
+function cleaner() {
+    return through.obj(function(file, enc, cb){
+        rimraf( path.resolve( (file.cwd || process.cwd()), file.path), function (err) {
+            if (err) {
+                this.emit('error', new gutil.PluginError('Cleanup old files', err));
+            }
+            this.push(file);
+            cb();
+        }.bind(this));
+    });
+}
+
+gulp.task("default", function() {
+    runSequence(['components', 'js', 'css'], 'rev', ['clean-js', 'clean-styles']);
+});
+
+gulp.task('css', function() {
+    return gulp.src([
+            assetsDir + '/components/bootstrap/dist/css/bootstrap.css',
+            assetsDir + '/components/font-awesome/css/font-awesome.css',
+            assetsDir + '/components/css-hamburgers/dist/hamburgers.css',
+            assetsDir + '/sass/**/*.scss'
+        ])
+        .pipe(sass({
+            errLogToConsole: true,
+            debugInfo: true,
+            lineNumbers: true,
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest(publicAssetsDir + '/css/'))
+        .pipe(notify('Sass Compiled!'));
+});
+
+gulp.task('components', function() {
+    return gulp.src([
+            assetsDir + '/components/jquery/dist/jquery.min.js',
+            assetsDir + '/components/bootstrap/dist/js/bootstrap.js',
+            assetsDir + '/components/handlebars/handlebars.min.js'
+        ])
+        .pipe(concat('components.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(publicAssetsDir + '/js/'))
+        .pipe(notify('Components Compiled!'));
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+            assetsDir + '/js/**/*.js'
+        ])
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(publicAssetsDir + '/js/'))
+        .pipe(notify('Javascript Compiled!'));
+});
+
+gulp.task('rev', function() {
+    return gulp.src([publicAssetsDir + '/css/**/*.css', publicAssetsDir + '/js/**/*.js'])
+        .pipe(rev())
+        .pipe(gulp.dest(publicAssetsDir + '/build/'))
+        .pipe(rev.manifest(publicAssetsDir + '/build/rev-manifest.json', {
+            base: publicAssetsDir + '/build/',
+            merge:true
+        }))
+        .pipe(gulp.dest(publicAssetsDir + '/build/'));
+});
+
+gulp.task('clean-js', function() {
+    return gulp.src( [publicAssetsDir + '/build/*.js'], {read: false})
+        .pipe( revOutdated() )
+        .pipe( cleaner() );
+});
+
+gulp.task('clean-styles', function() {
+    return gulp.src( [publicAssetsDir + '/build/*.css'], {read: false})
+        .pipe( revOutdated() )
+        .pipe( cleaner() );
+});
+
+gulp.task('watch', ['default'], function() {
+    gulp.watch(assetsDir + '/js/**/*.js', function() {
+        runSequence(['js'], 'rev', ['clean-js'])
+    });
+
+    gulp.watch(assetsDir + '/sass/**/*.scss', function() {
+        runSequence(['css'], 'rev', ['clean-styles'])
+    });
+});
---
+++ public/index.php
@@ -0,0 +1,71 @@
+<?php 
+
+// Get compiled asset
+function asset($file)
+{
+    $assets = json_decode(file_get_contents(__DIR__ . '/dist/rev-manifest.json'), true);
+    return $assets[$file];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Vault Innovation">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <title>Vault App</title>
+    
+    <!-- Custom styles for this template -->
+    <link href="<?php echo asset('app.css'); ?>" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="//oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button class="hamburger hamburger--spring navbar-toggle collapsed hide" type="button" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="hamburger-box">
+                  <span class="hamburger-inner"></span>
+                </span>
+            </button>
+
+            <a class="navbar-brand" href="#"><img src="/img/vault.png" class="logo"/></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Dashboard</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Users</a></li>
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+
+
+<div class="container">
+
+</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="<?php echo asset('components.js'); ?>"></script>
+<script src="<?php echo asset('app.js'); ?>"></script>
+
+</body>
+</html>
